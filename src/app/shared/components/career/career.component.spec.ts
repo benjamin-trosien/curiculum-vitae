bd1efcd242972ab714563b42c82bfa4812315837
Benjamin Trosien <benjamin.trosien@gmail.com>
2021-07-11 15:28:50 +0200

    trigger build pipeline

--- src/app/shared/components/career/career.component.spec.ts
+++ src/app/shared/components/career/career.component.spec.ts
@@ -1,25 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+    ComponentFixture,
+    TestBed,
+} from '@angular/core/testing';
 
 import { CareerComponent } from './career.component';
 
 describe('CareerComponent', () => {
-  let component: CareerComponent;
-  let fixture: ComponentFixture<CareerComponent>;
+    let component: CareerComponent;
+    let fixture: ComponentFixture<CareerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CareerComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [
+                CareerComponent,
+            ],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CareerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CareerComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
