9b81d5b3ea75d3004c888d0896475b8d907bbd70
Benjamin Trosien <benjamin.trosien@gmail.com>
2021-07-15 01:11:34 +0200

    more tests

--- src/app/shared/components/outline/outline.component.spec.ts
+++ src/app/shared/components/outline/outline.component.spec.ts
@@ -1,11 +1,26 @@
 import {
+    ChangeDetectionStrategy,
+    Component,
+    Input,
+} from '@angular/core';
+import {
     ComponentFixture,
     TestBed,
 } from '@angular/core/testing';
 
 import { OutlineComponent } from './outline.component';
 
-fdescribe('SkillsComponent', () => {
+@Component({
+    selector: 'app-rating-bar',
+    template: `
+        RATING_BAR RATING {{ rating | json }}
+    `
+})
+class MockRatingBarComponent {
+    @Input() rating: any;
+}
+
+describe('SkillsComponent', () => {
     let cut: OutlineComponent;
     let element: HTMLElement;
     let fixture: ComponentFixture<OutlineComponent>;
@@ -14,7 +29,11 @@ fdescribe('SkillsComponent', () => {
         await TestBed.configureTestingModule({
             declarations: [
                 OutlineComponent,
+
+                MockRatingBarComponent,
             ],
+        }).overrideComponent(OutlineComponent, {
+            set: { changeDetection: ChangeDetectionStrategy.Default },
         }).compileComponents();
     });
 
@@ -33,5 +52,49 @@ fdescribe('SkillsComponent', () => {
     it('should show nothing', () => {
 
         expect(element.innerText).toBe('');
+    });
+
+    describe('with input "person" set', () => {
+        beforeEach(() => {
+            cut.person = {
+                name: 'Nick Fury',
+                title: 'S.H.I.E.L.D Director',
+                photo: 'nick/fury.png',
+                personal: [ { superPower: 'coolness' } ],
+                skillList: [ {
+                    title: 'leading skills',
+                    items: [ { label: 'motivation', level: 'expert' } ],
+                } ],
+            } as any;
+            fixture.detectChanges();
+        });
+
+        it('should show name', () => {
+
+            expect(element.innerText).toContain('Nick Fury');
+        });
+
+        it('should show title', () => {
+
+            expect(element.innerText).toContain('S.H.I.E.L.D Director');
+        });
+
+        it('should show image', () => {
+
+            expect(element.querySelector('img')?.getAttribute('src')).toBe('nick/fury.png');
+        });
+
+        it('should show every entry of "personal" property', () => {
+
+            expect(element.innerText).toContain('superPower');
+            expect(element.innerText).toContain('coolness');
+        });
+
+        it('should show every entry of "skillList" property', () => {
+
+            expect(element.innerText).toContain('leading skills');
+            expect(element.innerText).toContain('motivation');
+            expect(element.innerText).toContain('expert');
+        });
     });
 });
